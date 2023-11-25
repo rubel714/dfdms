548ee599dcb400ae9042aa3432357ea9eba82cbc
anwar31dec <anwarcs36@yahoo.com>
2023-11-25 16:47:33 +0600

    home content and css changed

--- src/views/index-sections/Images.js
+++ src/views/index-sections/Images.js
@@ -8,17 +8,9 @@ import React from "react";
 function Images() {
   return (
     <>
-      <div>
+      <div className="mt-10">
         <p>
-          This is DFDMS home page. Lorem Ipsum is simply dummy text of the
-          printing and typesetting industry. Lorem Ipsum has been the industry's
-          standard dummy text ever since the 1500s, when an unknown printer took
-          a galley of type and scrambled it to make a type specimen book. It has
-          survived not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum.
+        An information system called Digital Field Data Monitoring System (DFDMS)- is developed in order to monitor the farmers and producer groups in the field. This application will be used from whole Bangladesh (up to Union level).
         </p>
         {/* <Container>
           <Row>
