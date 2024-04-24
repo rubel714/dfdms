db406a7ad05979e7445921e741676f8eaf8e4a83
rubel714 <rubel714@gmail.com>
2024-04-25 00:41:42 +0600

    new users password convert to hash

--- backend/source/api/api_pages/userpassconvertplaintohash.php
+++ backend/source/api/api_pages/userpassconvertplaintohash.php
@@ -4,20 +4,22 @@ include_once ('../../../env.php');
 include_once ('../pdolibs/pdo_lib.php');
 $dbh = new Db();
 
-$query = "SELECT * FROM t_users where UserId>=10;";
+$query = "SELECT * FROM t_users where UserId>=121000;";
 $resultdata = $dbh->query($query);
 
+$count=0;
 //echo "<pre>";
 foreach($resultdata as $key=>$row){
 	//print_r($row);	
-	
-	$Password = password_hash($row['Password'], PASSWORD_DEFAULT);
-	
-$sql = "update t_users set Password='".$Password."' where UserId=".$row['UserId'].";";
-
-$resultdata = $dbh->query($sql);
+	// echo strlen($row['Password'])."==";
+	if(strlen($row['Password'])<6){
+		$count++;
+		$Password = password_hash($row['Password'], PASSWORD_DEFAULT);
+		$sql = "update t_users set Password='".$Password."' where UserId=".$row['UserId'].";";
+		$resultdata = $dbh->query($sql);
+	}
 }
 
-echo "Updated:".count($resultdata);
+echo "Updated:".$count;
 
 ?>
